362813d424c4ca0a8cb4c292a4d9037b61b95c1c
Kiril Goguev <kgoguev@gmail.com>
2015-10-23 07:38:27 +0200

    test
    test

--- HappyNumbers.java
+++ HappyNumbers.java
@@ -3,15 +3,22 @@ public class HappyNumbers {
 	
 	public static void main(String [ ] args)
 	{
-		
+		//
 		for(int i=1;i<1001;i++)
 		{
 			if (isHappy(i))
-				System.out.println(i+"is happy");				
+				System.out.println(i+" is happy");				
 		}	
 		
 	}
 	
+	/**
+	 * This function checks if the number is happy, uses a trick to speed up execution since we don't explicitly have to check for a cycle (thanks wikipedia!)
+	 * Another way to detect cycles is to use a set as they do not allow duplicate numbers and just insert when testing the sum, but then the issue is that you have to keep a extra collection variable and refer to it as well as
+	 * Handle those exceptions.. exception handling is not fun.
+	 * @param num
+	 * @return
+	 */
 	public static boolean isHappy(int num)
 	{ 
 		
@@ -31,6 +38,8 @@ public class HappyNumbers {
 
 /**
  * Magical function that gets the sum of the squares.
+ * Any number sent in will use modulo to split the digits in the number .. so 49 mod 10 gives 9.. and subsequently dividing that number by 10 will give the next digit
+ * Another way to do this would be using Java's string and then split + parse but it is a naive way of doing it.
  * @param n
  * @param sum
  * @return the final sum
@@ -43,7 +52,7 @@ private static int getSumOfSquares(int n,int sum) {
 		{
 			extractedDigit=n%10;
 			sum+=extractedDigit*extractedDigit;
-			return getResultRec(n/10,sum);
+			return getSumOfSquares(n/10,sum);
 		}
 		return sum;
 		
